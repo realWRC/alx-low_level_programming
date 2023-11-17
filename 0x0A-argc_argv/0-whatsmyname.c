ee1bd14a7ed8e4f64bf815322c9819cc458c5b8b
Wongani <wonganichulu@gmail.com>
2023-11-17 21:56:53 +0200

    0x0A-argc_argv Task 0 fix

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ *main - prints the programs name.
+ *@argc: argument count
+ *@argv: arguments
+ *Return: Always 0
+ */
+
 int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
