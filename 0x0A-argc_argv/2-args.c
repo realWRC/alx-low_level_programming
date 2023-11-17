ad65d90677e74dbd31e3ec019b594e74b9ba1c56
Wongani <wonganichulu@gmail.com>
2023-11-17 22:17:02 +0200

    0x0A-argc_argv Task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints program name
+ * @argc: argument count
+ * @argv: arguments
+ * Return: Always (0)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
