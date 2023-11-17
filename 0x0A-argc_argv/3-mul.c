b0d8d8c88c1b07b70562f2a19046ec0a288a8ea2
Wongani <wonganichulu@gmail.com>
2023-11-17 22:37:02 +0200

    0x0A-argc_argv Task 3 fix

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,16 +10,17 @@
 
 int main(int argc, char *argv[])
 {
-	long int i = atoi(argv[1]) * atoi(argv[2]);
 	(void)argc;
 
 	if (argc < 3)
 	{
-		printf("Error");
+		printf("Error\n");
 		return (1);
 	}
 	else
 	{
+		long int i = atoi(argv[1]) * atoi(argv[2]);
+
 		printf("%li\n", i);
 		return (0);
 	}
