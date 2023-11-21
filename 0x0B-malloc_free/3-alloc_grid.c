91ef72500b9d3497d63c77ea887a598d248db810
Wongani <wonganichulu@gmail.com>
2023-11-21 22:33:40 +0200

    Added 3-alloc_grid.c of 0x0B-malloc_free fix

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -8,7 +8,7 @@ int **alloc_grid(int width, int height)
 	int i, j;
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	grid = (int **)malloc(sizeof(int) * height);
+	grid = (int **)malloc(sizeof(int *) * height);
 	if (grid == NULL)
 		return (NULL);
 	for (i = 0; i < height; i++)
