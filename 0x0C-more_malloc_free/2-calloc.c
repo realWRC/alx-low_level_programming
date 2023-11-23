bc80a62af544f30188c8047893c66317eeac8a87
Wongani <wonganichulu@gmail.com>
2023-11-23 12:07:42 +0200

    Added 2-calloc.c of 0x0C-more_malloc_free fix

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,7 +15,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	p = malloc(nmemb * size);
 	if (p == NULL)
 		return (NULL);
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < nmemb * size; i++)
 		((char *)p)[i] = 0;
 	return (p);
 }
