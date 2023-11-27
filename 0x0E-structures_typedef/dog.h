6d15efc60d024bbaf01eab46ea2419e78038f5be
Wongani <wonganichulu@gmail.com>
2023-11-27 17:30:58 +0200

    Added dog.h of 0x0E-structures_typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+/**
+ * struct dog - Data type containing dog information
+ * @name: dogs name
+ * @age: dogs age
+ * @owner: dogs owner
+ */
+struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+};
+
+#endif
