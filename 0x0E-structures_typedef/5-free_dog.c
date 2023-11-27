3cda651f39338c3400524de83c0e8681722ba2ca
Wongani <wonganichulu@gmail.com>
2023-11-27 21:43:48 +0200

    5-free_dog.c to 0x0E-structures_typedef fix

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -5,10 +5,12 @@
  * free_dog - frees memory allocated for a dog_t
  * @d: dog to free
  */
-
 void free_dog(dog_t *d)
 {
-	free(d->name);
-	free(d->owner);
-	free(d);
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
 }
