16d35ab2b1a5aa44d3c77390dfc1c13f5d661ef1
Wongani <wonganichulu@gmail.com>
2023-11-27 20:12:53 +0200

    4-new_dog.c to 0x0E-structures_typedef fix

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,18 +14,22 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *p;
+	char *s1;
+	char *s2;
 
+	s1 = strdup(name);
+	s2 = strdup(owner);
 	p = malloc(sizeof(dog_t));
 	if (p == NULL)
 		return (NULL);
-	p->name = strdup(name);
+	p->name = s1;
 	if (p->name == NULL)
 	{
 		free(p);
 		return (NULL);
 	}
 	p->age = age;
-	p->owner = strdup(owner);
+	p->owner = s2;
 	if (p->owner == NULL)
 	{
 		free(p);
