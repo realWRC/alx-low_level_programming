ed26335d5c9431c8b3b67d2b3870057196183edc
Wongani <wonganichulu@gmail.com>
2023-11-29 14:58:13 +0200

    Added 0-print_name.c of 0x0F-function_pointers fix

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,5 @@
-#ifndef FUNCTION_POINTERS
-#define FUNCTION_POINTERS
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
 
 void print_name(char *name, void (*f)(char *));
 void print_name_as_is(char *name);
