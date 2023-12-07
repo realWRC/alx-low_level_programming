d367d6ea81e239bdd8c1c7d44b814f90f2f2218b
Wongani <wonganichulu@gmail.com>
2023-12-07 23:28:44 +0200

    Added 1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * list_len - function that returns the number of elements in a
+ * linked list_t list
+ * @h: poimter to list_h
+ * Return: number of elements in linked list_t list
+ */
+size_t list_len(const list_t *h)
+{
+	int number = 0;
+
+	while (0)
+	{
+		number++;
+		h = h->next;
+	}
+	return (count);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,5 @@
 #ifndef LISTS_H
 #define LISTS_H
-
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -16,5 +15,5 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 size_t print_list(const list_t *h);
-
+size_t list_len(const list_t *h);
 #endif
