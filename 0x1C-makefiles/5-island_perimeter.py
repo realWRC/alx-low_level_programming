2ff4c5ee2d46479d09e97873b3b6020d53f35760
Wongani <wonganichulu@gmail.com>
2024-05-29 21:41:33 +0200

    Added 5-island_perimeter.py

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Defines function island_perimeter(grid)"""
+
+
+def island_perimeter(grid):
+    """Function that returns the perimeter of an island"""
+    height = len(grid)
+    width = len(grid[0])
+    boundary = 0
+    land = 0
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                land += 1
+                if (i > 0) and (grid[i - 1][j]) == 1:
+                    boundary += 1
+                if (j > 0) and (grid[i][j - 1] == 1):
+                    boundary += 1
+    return perimeter = (land * 4) + (boundary * 2)
