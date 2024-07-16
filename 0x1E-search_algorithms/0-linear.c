e6d484175c10d0604ba6f6c8bc65b7d885325aef
Wongani <wonganichulu@gmail.com>
2024-07-16 12:03:32 +0200

    0-linear.c: Added fixes to print process

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -10,17 +10,19 @@
 int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
+
 	if (array == NULL)
 	{
-		return(-1)
+		return (-1);
 	}
 
 	for (i = 0; i < size; i++)
 	{
+		printf("Value checked array[%lu] = [%i]\n", i, array[i]);
 		if (array[i] == value)
 		{
-			return(i);
+			return (i);
 		}
 	}
-	return(-1);
+	return (-1);
 }
